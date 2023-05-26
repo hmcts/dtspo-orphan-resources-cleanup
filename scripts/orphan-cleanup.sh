dacf3f80f8a9e2cedc6c237bd07ae83a8733148d
Rees Pozzi <reespozzi@icloud.com>
2023-05-26 15:22:41 +0100

    Add resource limit back for testing checkboxg

--- scripts/orphan-cleanup.sh
+++ scripts/orphan-cleanup.sh
@@ -70,7 +70,7 @@ resources_to_delete=$(sed 's/""/" "/g' <<< $resources_to_delete)
 
 # Convert into array to loop over resources and sequentially (to record failures) delete them
 resources_to_delete=($resources_to_delete)
-for resource in "${resources_to_delete[@]}"
+for resource in "${resources_to_delete[@]:0:1}"
 do
   # Trim " from resource, as az command also wraps with '
   resource=$(echo $resource | tr -d '"')
