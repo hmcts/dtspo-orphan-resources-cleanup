46ed5c89c9572b6287e3933dcf9aaa4970de9c9c
Daniel Wilson <daniel.wilson@kainos.com>
2023-08-21 16:02:09 +0100

    fix syntax

--- components/custom_roles/roles.tf
+++ components/custom_roles/roles.tf
@@ -1,9 +1,9 @@
 resource "azurerm_role_definition" "orphan_cleanup" {
-  for_each = var.management_groups
+  for_each = toset(var.management_groups)
 
   name        = "Orphan Resource Cleanup Read/Delete"
   description = "Read and Resource Delete Access to applicably assigned scope"
-  scope       = "/providers/Microsoft.Management/managementGroups/"[each.value]
+  scope       = "/providers/Microsoft.Management/managementGroups/" [each.key]
 
   permissions {
     actions     = ["*/read", "Microsoft.Resources/*/delete"]
