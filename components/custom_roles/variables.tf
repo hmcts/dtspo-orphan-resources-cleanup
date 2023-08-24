2a50377b632dc902d1534e300ef12251fcdb950b
Daniel Wilson <daniel.wilson@kainos.com>
2023-08-24 13:39:22 +0100

    update vars

--- components/custom_roles/variables.tf
+++ components/custom_roles/variables.tf
@@ -8,30 +8,8 @@ variable "env" {
   default = []
 }
 
-variable "project" {
-  default = "hmcts"
-}
-
-variable "product" {
-}
-
 variable "builtFrom" {
 }
 
-variable "location" {
-  default = "UK South"
-}
-
-variable "common_tags" {
-  default = []
-}
-
-variable "expiresAfter" {
-  description = "Date when Sandbox resources can be deleted. Format: YYYY-MM-DD"
-  default     = "3000-01-01"
-}
-
-variable "management_groups" {
-  type    = list(string)
-  default = []
+variable "product" {
 }
